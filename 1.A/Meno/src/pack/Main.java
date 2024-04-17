b3123e4154ef5b387c2b038a8b16b790f9578771
MichaelaMarsalkova <167296562+MichaelaMarsalkova@users.noreply.github.com>
2024-04-17 11:02:46 +0200

    Add files via upload

---
+++ 1.A/Meno/src/pack/Main.java
@@ -0,0 +1,16 @@
+package pack;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int cislo;
+		System.out.println("Zadaj cislo");
+		cislo = sc.nextInt();
+		System.out.println(cislo);
+
+	}
+
+}
